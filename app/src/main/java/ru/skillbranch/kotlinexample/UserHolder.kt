17ed60c3d54386a5d8f931bee94cfd1df1db3d6c
sanyok953 <sanyok9531@gmail.com>
2021-06-21 00:58:57 +0500

    Done

--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -1,5 +1,34 @@
 package ru.skillbranch.kotlinexample
 
-object UserHolder {
+import androidx.annotation.VisibleForTesting
 
+object UserHolder {
+    private val map = mutableMapOf<String, User>()
+
+    fun registerUser(
+        fullName: String,
+        email: String,
+        password: String
+    ): User = User.makeUser(fullName, email = email, password = password)
+        .also { user ->
+            if (user.login in map)
+                throw IllegalArgumentException("A user with this email already exists")
+            else
+                map[user.login] = user
+        }
+
+    fun loginUser(login: String, password: String): String? =
+        map[login.trim()]?.let {
+            if (it.checkPassword(password)) it.userInfo
+            else null
+        }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    fun clearHolder() {
+        map.clear()
+    }
+
+    fun registerUserByPhone(fullName: String, rawPhone: String): User =
+        User.makeUser(fullName, rawPhone)
+            .also { user -> map[user.login] = user }
 }
